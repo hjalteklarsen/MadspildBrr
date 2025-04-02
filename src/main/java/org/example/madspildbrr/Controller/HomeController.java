2ffa74ea7e709335dfa81cec8e6aab1ff7140ba9
hjalteklarsen <hjalteklarsen@gmail.com>
2025-04-02 10:21:33 +0200

    Initial

---
+++ src/main/java/org/example/madspildbrr/Controller/HomeController.java
@@ -0,0 +1,7 @@
+package org.example.madspildbrr.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+}
---
+++ src/main/java/org/example/madspildbrr/MadspildBrrApplication.java
@@ -0,0 +1,13 @@
+package org.example.madspildbrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadspildBrrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadspildBrrApplication.class, args);
+    }
+
+}
