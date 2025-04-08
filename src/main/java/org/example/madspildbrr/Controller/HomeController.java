9a905c45694bd518f21a4fd3c6e3e35bbca20b52
shuresm57 <vsorensen31@gmail.com>
2025-04-08 11:26:15 +0200

    logo

--- src/main/java/org/example/madspildbrr/Controller/HomeController.java
+++ src/main/java/org/example/madspildbrr/Controller/HomeController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String index()
-    {
-        return "home/create-raffle";
+    public String index() {
+        return "home/index";
     }
 
     @GetMapping("/event")
